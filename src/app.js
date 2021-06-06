5045080e7e97849e64cfd90f781d1116bd5adbf9
baiju <baiju28feb@gmail.com>
2021-06-06 19:33:31 +0530

    About section

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+//hamburger button start//
 const hamburgerButton = document.getElementById('hamburger')
 const navlist = document.getElementById('nav-list')
 
@@ -7,7 +8,8 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton  )
-
+//hamburger button end//
+//Type writer effect begin
 const skills = ['Software Engineer', 'Web Developer', 'Back-End Developer' ];
 let count = 0;
 let item = 0;
@@ -28,3 +30,37 @@ let letter= '';
   }
   setTimeout(type, 300);
 })();
+//Type writer effect End
+
+//Scroll smooth begings
+$(".button").click(function() {
+  $('html,body').animate({
+      scrollTop: $("#about-me").offset().top},
+      'slow');
+});
+
+$("#aboutme").click(function(){
+  $('html,body').animate({
+    scrollTop: $("#about-me").offset().top},
+    'slow');
+  });
+  $("#myprojects").click(function(){
+    $('html,body').animate({
+      scrollTop: $("#projects").offset().top},
+      'slow');
+    });
+
+    $("#contactme").click(function(){
+      $('html,body').animate({
+        scrollTop: $("#contact-me").offset().top},
+        'slow');
+      });
+    //Scroll smooth begings End
+      window.addEventListener("scroll", function(){
+        var header = document.querySelector("nav");
+        header.classList.toggle("sticky", window.scrollY > 0);
+      })
+    //sticky navbar begin
+
+
+    //sticky navbar end
