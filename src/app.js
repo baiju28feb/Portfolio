282d3def6ea7ff47284e7be61b657341599a3e69
baiju <baiju28feb@gmail.com>
2021-06-06 10:58:30 +0530

    Blinking text

--- src/app.js
+++ src/app.js
@@ -7,3 +7,24 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton  )
+
+const skills = ['Software Engineer', 'Web Developer', 'Back-End Developer' ];
+let count = 0;
+let item = 0;
+let currentitem = '';
+let letter= '';
+
+(function type (){
+  if(skills.length === count)
+    {
+      count = 0;
+    }
+  currentitem = skills[count];
+  letter = currentitem.slice(0, ++item);  
+  document.querySelector(".type").textContent = letter;
+  if(currentitem.length === item){
+    count ++;
+    item = 0;
+  }
+  setTimeout(type, 300);
+})();
